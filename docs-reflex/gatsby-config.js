7effb29168bded1447ea798088f578c20211f759
Jason Bahl <jasonbahl@mac.com>
2020-09-08 12:34:01 -0600

    - Update url to use environment variable

--- docs-reflex/gatsby-config.js
+++ docs-reflex/gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     {
       resolve: `gatsby-source-wordpress-experimental`,
       options: {
-        url: `http://wpgqldocs.local/graphql`,
+        url: process.env.WPGRAPHQL_URL,
       },
     },
   ],
