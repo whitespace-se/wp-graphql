3d058ff128d6ca95ea381745bc0d03a301d3549c
Jason Bahl <jasonbahl@mac.com>
2020-09-30 22:14:10 -0600

    Update single-doc.js

--- docs-reflex/src/templates/docs/single-doc.js
+++ docs-reflex/src/templates/docs/single-doc.js
@@ -4,8 +4,8 @@ import { Layout } from "@reflexjs/gatsby-theme-core"
 import { Doc } from "./doc"
 
 export default ({ data }) => (
-  <Layout pathname={data.doc.uri}>
-    <Doc {...data.doc} previousDoc={data.prev} nextDoc={data.next} />
+  <Layout pathname={data.doc.uri ? data.doc.uri : data.doc.id}>
+    <Doc {...data.doc} previousDoc={data.prev ? data.prev : null} nextDoc={data.next ? data.next : null} />
   </Layout>
 )
 
